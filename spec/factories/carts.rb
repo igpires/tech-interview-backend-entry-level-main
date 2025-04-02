28512d745b93477fabf05e8ff8ac8618bf1749d7
Igor <pires.igor2@gmail.com>
2025-04-02 16:23:58 -0300

    feat(factory): add cart and shopping_cart factories with default attributes

---
+++ spec/factories/carts.rb
@@ -0,0 +1,10 @@
+# spec/factories/carts.rb
+FactoryBot.define do
+  factory :cart do
+    total_price { 0.0 }
+    abandoned { false }
+    last_interaction_at { Time.current }
+  end
+
+  factory :shopping_cart, parent: :cart
+end
